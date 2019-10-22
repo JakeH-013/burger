bf359e8bf9581960cf6f04700723495c40dd2ffe
Jeffrey Huddleston <jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.comgit config --global user.email jacobjhudd@gmail.com>
2019-10-22 19:21:20 -0400

    created the routes and connected them to the server. server connects and works without error

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,5 +1,42 @@
-var express = require("experss");
+var express = require("express");
 
 var router = express.Router();
 
 var burger = require("../models/burger.js");
+
+router.get("/", (req, res) => {
+    burger.showAll((data) => {
+        var handlebarObj = {
+            burger: data
+        };
+        console.log(handlebarObj);
+        res.render("index", handlebarObj);
+    });
+});
+
+router.post("/api/burgers", (req, res) => {
+    burger.createBurger(
+        ["burger name", "devoured"],
+        [req.body.name, req.body.devoured],
+        (result) => {
+            res.json({ id: result.insertId });
+        });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+    var status = "id " + req.params.id;
+
+    console.log("condition", status);
+
+    burger.updateStatus(
+        {devoured: req.body.devoured},
+        status, (result) => {
+            if (result.changedRows ==0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
+});
+
+module.exports = router;
